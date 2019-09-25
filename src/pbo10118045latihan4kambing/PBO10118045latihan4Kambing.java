825b4f9f92b1fb6cc75c43c9b7bd230db850e572
Ilham <Ilham@LAPTOP-G4VGSH61>
2019-09-25 20:43:35 +0700

    first commit

---
+++ src/pbo10118045latihan4kambing/PBO10118045latihan4Kambing.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10118045latihan4kambing;
+
+/**
+ *
+ * @author 
+ * Nama     :   Muhammad Ilham Apriyadi
+ * Kelas    :   IF 2
+ * NIM      :   10118045
+ * Deskripsi Program : Kambing
+ */
+public class PBO10118045latihan4Kambing {
+
+    public void tambahKambing() {
+        //Deklarasi Varabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah ditambah: " +jumlahKambing);
+        
+        
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PBO10118045latihan4Kambing kambingJantan = new PBO10118045latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+
+    
+    
+}
